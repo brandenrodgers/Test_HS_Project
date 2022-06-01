cfb1f225f83abcf18faa98f781539aee3a6b82e2
Branden Rodgers <branden@rodgersworld.com>
2022-06-01 15:20:18 -0400

    Create gh-card.js

---
+++ src/gh-app/app.functions/gh-card.js
@@ -0,0 +1,20 @@
+exports.main = async (context = {}, sendResponse) => {
+  const staticCard = {
+    objectId: 1,
+    title: 'Sample data',
+    data: "Some random data to display",
+  };
+
+  try {
+    sendResponse({
+      results: [staticCard],
+      primaryAction: {
+        type: 'SERVERLESS_ACTION_HOOK',
+        serverlessFunction: 'gh-card',
+        label: 'GH-Created Card',
+      },
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
